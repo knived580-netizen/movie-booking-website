92ac50d4798f4412051c695ca1ed054ff6217dfc
Nived K <knived580@gmail.com>
2026-03-17 19:51:12 +0530

    auto create admin

--- moviebooking/settings.py
+++ moviebooking/settings.py
@@ -148,3 +148,10 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
